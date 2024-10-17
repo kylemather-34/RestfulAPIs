a353817a10e8ca46e2b725932301a90da20bb35c
Miki Tebeka <miki@353solutions.com>
2024-10-17 08:55:48 +0300

    validate & simpler

--- Ch05/05_01/server.py
+++ Ch05/05_01/server.py
@@ -10,15 +10,18 @@ def health():
 
 
 if __name__ == '__main__':
-    from config import settings
-    import uvicorn
-
     from argparse import ArgumentParser
+
+    import uvicorn
+    from config import settings
 
     parser = ArgumentParser()
     parser.add_argument('--port', type=int, default=settings.port)
     args = parser.parse_args()
 
     settings.update(vars(args))
+
+    if settings.port < 0 or settings.port > 65_535:
+        raise SystemExit(f'error: invalid port - {settings.port}')
 
     uvicorn.run(app, port=settings.port)
